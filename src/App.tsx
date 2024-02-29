74410b37c58c7663dce9c5844798ea93aafacbcc
marina-mmap <marinapereira.mmap@gmail.com>
2024-02-29 09:52:20 -0300

    atualização de imagens

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
+import './App.css';
 import 'react-toastify/dist/ReactToastify.css';
 import Login from './paginas/login/Login';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
